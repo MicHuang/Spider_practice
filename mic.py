24455494f97eeeed5908b006af0ac5cff0ffbcce
Mickey <ekey.huang@gmail.com>
2016-12-10 18:18:31 -0500

    change the output from print to write file

--- mic.py
+++ mic.py
@@ -1,11 +1,27 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import os
 
 url_home = 'https://www.giuem.com/'
+folder = 'Giuem'
+file_name = 'title'
 
 
-def crawl_title(url):
+def create_dir_file(directory, file_name):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    f = open(directory + '/' + file_name + '.txt', 'w')
+    f.close()
+    print('file created.')
+
+
+def append_file(path, data):
+    with open(path, 'a') as f:
+        f.write(data + '\n')
+
+
+def crawl_title(url, directory, content='title'):
 
     print('Opening the webpage...')
     response = urlopen(url)
@@ -22,18 +38,19 @@ def crawl_title(url):
     for node in nodes:
         title = node.string
         link = url + str(node['href'])
-        print(title)
-        print(link)
-        print()
+        append_file(directory + '/' + content + '.txt', title)
+        append_file(directory + '/' + content + '.txt', link + '\n')
+    print('Done!')
 
 
-def crawl_page_title(max_page):
+def crawl_page_title(max_page, directoy, content):
     page = 2
     url = 'https://www.giuem.com/page/' + str(page)
     while page <= max_page:
-        crawl_title(url)
+        crawl_title(url, directoy, content)
         page += 1
 
 
-crawl_title(url_home)
-crawl_page_title(4)
+create_dir_file(folder, file_name)
+crawl_title(url_home, folder, file_name)
+crawl_page_title(3, folder, file_name)
